b73f654ef6e8c1880c5243fd18fe55360764db2b
TEYEMLONG-NGUIMFACK Robert-Martial <robert-martial.teyemlong-nguimfack@akka.eu>
2019-05-09 21:07:16 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+// Importing the pagination module for the application.
+import { NgxPaginationModule } from 'ngx-pagination';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgxPaginationModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
